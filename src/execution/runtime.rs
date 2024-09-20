51bfad50630ded632338397c4bc4d8744ef6c2fa
Kirill Elizarov <kirill.elizarov@metalamp.io>
2024-09-20 16:58:16 +0700

    Implemeted function call

--- src/execution/runtime.rs
+++ src/execution/runtime.rs
@@ -1,6 +1,10 @@
-use anyhow::bail;
+use anyhow::{anyhow, bail};
 
-use crate::binary::{instruction::Instruction, module::Module, types::ValueType};
+use crate::binary::{
+    instruction::Instruction,
+    module::Module,
+    types::{ExportDesc, ValueType},
+};
 
 use super::{
     store::{FuncInst, InternalFuncInst, Store},
@@ -33,7 +37,21 @@ impl Runtime {
         })
     }
 
-    pub fn call(&mut self, idx: usize, args: Vec<Value>) -> Result<Option<Value>, anyhow::Error> {
+    pub fn call(
+        &mut self,
+        name: impl Into<String>,
+        args: Vec<Value>,
+    ) -> Result<Option<Value>, anyhow::Error> {
+        let idx = match self
+            .store
+            .module
+            .exports
+            .get(&name.into())
+            .ok_or(anyhow!("not found export function"))?
+            .desc
+        {
+            ExportDesc::Func(idx) => idx as usize,
+        };
         let Some(func_inst) = self.store.funcs.get(idx) else {
             bail!("not found func")
         };
@@ -45,7 +63,7 @@ impl Runtime {
         }
     }
 
-    fn invoke_internal(&mut self, func: InternalFuncInst) -> Result<Option<Value>, anyhow::Error> {
+    fn push_frame(&mut self, func: &InternalFuncInst) {
         let bottom = self.stack.len() - func.func_type.params.len();
         let mut locals = self.stack.split_off(bottom);
 
@@ -55,7 +73,6 @@ impl Runtime {
                 ValueType::I64 => locals.push(Value::I64(0)),
             }
         }
-
         let arity = func.func_type.results.len();
 
         let frame = Frame {
@@ -67,6 +84,12 @@ impl Runtime {
         };
 
         self.call_stack.push(frame);
+    }
+
+    fn invoke_internal(&mut self, func: InternalFuncInst) -> Result<Option<Value>, anyhow::Error> {
+        let arity = func.func_type.results.len();
+
+        self.push_frame(&func);
 
         if let Err(e) = self.execute() {
             self.cleanup();
@@ -115,6 +138,14 @@ impl Runtime {
                     let result = left + right;
                     self.stack.push(result);
                 }
+                Instruction::Call(idx) => {
+                    let Some(func) = self.store.funcs.get(*idx as usize) else {
+                        bail!("not found func");
+                    };
+                    match func {
+                        FuncInst::Internal(func) => self.push_frame(&func.clone()),
+                    }
+                }
             }
         }
         Ok(())
@@ -153,7 +184,30 @@ mod tests {
 
         for (left, right, want) in tests {
             let args = vec![Value::I32(left), Value::I32(right)];
-            let result = runtime.call(0, args)?;
+            let result = runtime.call("add", args)?;
+            assert_eq!(result, Some(Value::I32(want)));
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn not_found_export_function() -> Result<(), anyhow::Error> {
+        let wasm = wat::parse_file("src/fixtures/func_add.wat")?;
+        let mut runtime = Runtime::instantiate(wasm)?;
+        let result = runtime.call("foooooo", vec![]);
+        assert!(result.is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn func_call() -> Result<(), anyhow::Error> {
+        let wasm = wat::parse_file("src/fixtures/func_call.wat")?;
+        let mut runtime = Runtime::instantiate(wasm)?;
+        let tests = vec![(2, 4), (10, 20), (1, 2)];
+
+        for (arg, want) in tests {
+            let args = vec![Value::I32(arg)];
+            let result = runtime.call("call_doubler", args)?;
             assert_eq!(result, Some(Value::I32(want)));
         }
         Ok(())
